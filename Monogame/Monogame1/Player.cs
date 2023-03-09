36c16103b80ee5788120535da4303f7b4e91bb58
unknown <a25959@alunos.ipca.pt>
2023-03-09 21:49:53 +0000

    segundo commit

---
+++ Monogame/Monogame1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Monogame1
+{
+    public class Player
+    {
+        private Point position;
+        public Point Position => position;
+        public Player(int x, int y) //constructor que dada a as +osições guarda a sua posição
+        {
+            position = new Point(x, y);
+        }
+    }
+}
---
+++ Monogame/Monogame1/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame1.Game1();
+game.Run();
